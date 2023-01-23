1578e81c75bf4a07add2ae6e44f5f47ffd7b6e39
Eigentlich1 <123163056+Eigentlich1@users.noreply.github.com>
2023-01-24 02:02:21 +0300

    Delete Новая папка (4)/T1 directory

--- Новая папка (4)/T1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.WriteLine("Введите трёхзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int res = (number % 100) / 10;
-System.Console.WriteLine(res);
