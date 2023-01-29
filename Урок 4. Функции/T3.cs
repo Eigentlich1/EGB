6fadb15f59c1b860cf80e59f5012236eedeb7822
Eigentlich1 <123163056+Eigentlich1@users.noreply.github.com>
2023-01-29 19:41:33 +0300

    Add files via upload

---
+++ Урок 4. Функции/T3.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int lenghtArray = ReadInt("Введите длинну массива ");
+
+int[] randomArray = new int[lenghtArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+int ReadInt(string m)
+{
+    Console.Write(m);
+    return Convert.ToInt32(Console.ReadLine());
+}
