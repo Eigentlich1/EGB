a3e14ffcc6eb7fa46135dbe0e06fcc18414e1297
Eigentlich1 <123163056+Eigentlich1@users.noreply.github.com>
2023-01-29 20:18:48 +0300

    Add files via upload

---
+++ Урок 5. Массивы/T2.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Введите размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int summ = 0;
+
+for (int a = 0; a < numbers.Length; a+=2)
+    summ = summ + numbers[a];
+
+    Console.WriteLine(summ);
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write(" ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write(" ");
+    Console.WriteLine();
+}
