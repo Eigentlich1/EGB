ad7a210b6a07bf77a41ad2ceb6d315b9926dd6c9
Eigentlich1 <123163056+Eigentlich1@users.noreply.github.com>
2023-01-29 19:03:58 +0300

    Add files via upload

---
+++ Урок 4. Функции/T1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+GetExtentnt (num1, num2);
+
+void GetExtentnt (int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+    Console.WriteLine(result);
+}
