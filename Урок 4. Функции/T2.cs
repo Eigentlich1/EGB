e8083b83d34801fed3ad44d5458296adc7e9d57e
Eigentlich1 <123163056+Eigentlich1@users.noreply.github.com>
2023-01-29 19:38:04 +0300

    Add files via upload

---
+++ Урок 4. Функции/T2.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int number = ReadInt("Введите число ");
+
+int lenght = NumberLen(number);
+Summ(number, lenght);
+int NumberLen(int a)
+{
+    int i = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        i++;
+    }
+    return i;
+}
+void Summ(int num, int lenght)
+{
+    int sum = 0;
+    for (int i = 1; i <= lenght; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    Console.WriteLine(sum);
+}
+int ReadInt(string mes)
+{
+    Console.Write(mes);
+    return Convert.ToInt32(Console.ReadLine());
+}
